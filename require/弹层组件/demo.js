091e2bd728b129c63a59785cd1fb2339dfbc6a21
wuying <wuy_123@126.com>
2017-02-20 10:00:57 +0800

    require

--- require/弹层组件/demo.js
+++ require/弹层组件/demo.js
@@ -8,13 +8,14 @@
 //});
 require(['jquery','dialog'],function($,Dialog){
     $('#btn').on('click',function(){
-        Dialog.open({
+        var dialog = new Dialog();
+        dialog.open({
             width:500,
             title:"登录",
             url:'login.html'
         });
     });
     $('#btn2').on('click',function(){
-        Dialog.close();
+        dialog.close();
     });
 });

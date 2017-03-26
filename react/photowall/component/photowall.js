9ba18bc6cedcc5b5a215b5d308de3e0723f37904
xiecheng <13136666328@163.com>
2017-03-26 14:57:40 +0800

    reactjs例子

--- react/photowall/component/photowall.js
+++ react/photowall/component/photowall.js
@@ -1,16 +1,26 @@
 var ImgFigure = React.createClass({
+    handleClick : function(){
+        if(this.props.imgFigureArr.isCenter){
+            this.props.inverse();
+        }else{
+            this.props.center();
+        }
+    },
     render: function () {
         var styleObj = {
             top: this.props.imgFigureArr.pos.top,
             left: this.props.imgFigureArr.pos.left,
             transform: "rotate(" + this.props.imgFigureArr.rotate + "deg)"
         };
+        if(this.props.imgFigureArr.isInverse){
+            styleObj.transform = "rotateY(180deg)";
+        }
         return (
-            <figure className="img-figure" style={styleObj}>
+            <figure className="img-figure" style={styleObj} onClick={this.handleClick}>
                 <img src={"img/" + this.props.data.fileName}/>
                 <figcaption>
                     <h2>{this.props.data.title}</h2>
-                    <h4></h4>
+                    <div>{this.props.data.desc}</div>
                 </figcaption>
             </figure>
         );
@@ -18,9 +28,23 @@ var ImgFigure = React.createClass({
 });
 
 var Controller = React.createClass({
+    handleClick : function(){
+        if(this.props.imgFigureArr.isCenter){
+            this.props.inverse();
+        }else{
+            this.props.center();
+        }
+    },
     render: function () {
+        var ctrlClass = "ctrl";
+        if(this.props.imgFigureArr.isCenter){
+            ctrlClass += " is-center"
+            if(this.props.imgFigureArr.isInverse){
+                ctrlClass += " is-inverse";
+            }
+        }
         return (
-            <span></span>
+            <span className={ctrlClass} onClick={this.handleClick}></span>
         );
     }
 });
@@ -33,7 +57,9 @@ var Photowall = React.createClass({
                     top: 0,
                     left: 0
                 },
-                rotate: 0
+                rotate: 0,
+                isCenter: false,
+                isInverse: false
             }]
         };
     },
@@ -74,7 +100,9 @@ var Photowall = React.createClass({
             yMin: -hHalfImgFigureDOM,
             yMax: hStageDOM - hHalfImgFigureDOM
         };
-
+        this.rearrage(0);
+    },
+    rearrage : function(centerIdx){
         var imgFigureInfo = this.state.imgFigureArr;
         for (var i = 0; i < imgFigureInfo.length; i++) {
             if (i < imgFigureInfo.length / 2) {
@@ -89,13 +117,29 @@ var Photowall = React.createClass({
                 };
             }
             imgFigureInfo[i].rotate = getRandom(-30, 30);
+            imgFigureInfo[i].isCenter = false;
+            imgFigureInfo[i].isInverse = false;
         }
-        imgFigureInfo[0].pos = this.const.centerPos;
-        imgFigureInfo[0].rotate = 0;
+        imgFigureInfo[centerIdx].pos = this.const.centerPos;
+        imgFigureInfo[centerIdx].rotate = 0;
+        imgFigureInfo[centerIdx].isCenter = true;
 
         this.setState({
             imgFigureArr: imgFigureInfo
         });
+    },
+    center : function(centerIdx){
+        return function(){
+            this.rearrage(centerIdx);
+        }.bind(this);
+    },
+    inverse : function(centerIdx){
+        return function(){
+            this.state.imgFigureArr[centerIdx].isInverse = !this.state.imgFigureArr[centerIdx].isInverse;
+            this.setState({
+                imgFigureArr: this.state.imgFigureArr
+            });
+        }.bind(this);
     },
     render: function () {
         var imgArr = [];
@@ -108,12 +152,16 @@ var Photowall = React.createClass({
                         top: 0,
                         left: 0
                     },
-                    rotate: 0
+                    rotate: 0,
+                    isCenter: false,
+                    isInverse: false
                 }
             }
             imgArr.push(<ImgFigure key={index} data={value}
-                                   imgFigureArr={this.state.imgFigureArr[index]} ref="imgFigure"/>);
-            ctrlArr.push(<Controller key={index}/>);
+                                   imgFigureArr={this.state.imgFigureArr[index]} ref="imgFigure"
+                                   center={this.center(index)} inverse={this.inverse(index)}/>);
+            ctrlArr.push(<Controller key={index} imgFigureArr={this.state.imgFigureArr[index]}
+                                     center={this.center(index)} inverse={this.inverse(index)}/>);
         }.bind(this));
         return (
             <section className="stage" ref="stage">
